540a4465026a09f8af5bd3725483d73b42ce9974
Volodymyr Lykhvar <vlykhvar@gmail.com>
2021-02-09 10:04:57 +0200

    changes by Sofia request2

--- src/main/java/dao/impl/UserDaoImpl.java
+++ src/main/java/dao/impl/UserDaoImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-
     private final SessionFactory sessionFactory;
 
     @Autowired
--- src/main/java/service/impl/UserServiceImpl.java
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -9,8 +9,12 @@ import service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+
     @Autowired
-    private UserDao userDao;
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void add(User user) {
